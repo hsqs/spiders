accaab971b1d8f31d5b03bf2b164679988fae08b
hsqs <wangsheng@wangshengdeMBP.lan>
2016-04-10 01:24:04 +0800

    add a spider of website 'one'
    this script will download all of its picture under site-address/one/*

---
+++ one/script/main.py
@@ -0,0 +1,39 @@
+import requests
+import bs4
+import urllib3
+
+
+START_PAGE_NUM = 14
+MAX_PAGE_NUM = 1302
+
+http = urllib3.PoolManager()
+
+
+def get_soup():
+
+    for index in range(START_PAGE_NUM, MAX_PAGE_NUM + 1):
+        page_url = "http://wufazhuce.com/one/{}".format(index)
+        response = requests.get(page_url)
+        if response.status_code != 200:
+            print("{}:{}".format(page_url, response.status_code))
+            continue
+
+        soup = bs4.BeautifulSoup(response.text, "html.parser")
+        divs = soup.select("div")
+        for div in divs:
+            try:
+                if "one-cita" in div.attrs["class"]:
+                    contents = ''.join(div.contents).strip()
+                    img_url = soup.find_all("img")[1]['src']
+
+                    img_request = http.request("GET", img_url)
+                    with open('../downloads/{}.jpg'.format(contents), 'wb') as jpg:
+                        jpg.write(img_request.data)
+                        print("{}:{}".format(page_url, 'done'))
+            # if div not contains a key attr, continue
+            except KeyError:
+                continue
+
+
+if __name__ == "__main__":
+    get_soup()
