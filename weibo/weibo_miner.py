417cc13c900210423c489d2868a73d61f9bb3414
hsqs <wangsheng@wangshengdeMBP.lan>
2016-04-10 18:38:01 +0800

    get multi users picture
    every user’s image are stored in a specific folder

--- weibo/weibo_miner.py
+++ weibo/weibo_miner.py
@@ -11,36 +11,40 @@ def get_exist_names():
 
 
 exists = get_exist_names()
-user_id = '2663489000'
+users = {1303132300: 2, 2663489000: 4, 5021784365: 3}
 cookie_values1 = ['_T_WM=e1eadf753daa8e0eaa6a98dbd924dd89;', ' SUB=_2A256DbneDeRxGedJ6FsV9CjLzjiIHXVZ8ceWrDV6PUJb']
 cookie_values2 = ['rdBeLVP8kW1LHeuE-tglgN4hADdeZV_zCKufEX1tGA..;', ' SUHB=0WGdcIcQ6fIJa7; SSOLoginState=1460259214']
 cookie = {"Cookie": ''.join(cookie_values1) + ''.join(cookie_values2)}
 
 
 def miner():
-    page_num = 2
-    for page in range(1, page_num + 1):
-        url = 'http://weibo.cn/u/%s?filter=1&page=%d' % (user_id, page)
-        content = requests.get(url, cookies = cookie).content
-        soup = BeautifulSoup(content, "html.parser")
-        download_one_page(soup)
+    for user_id, page_num in users.items():
+        path_img = 'downloads/{}'.format(user_id)
+        if not os.path.exists(path_img):
+            os.makedirs(path_img)
 
-        # more pic link, load and download again
-        img_links = soup.find_all('a')
-        for link in img_links:
-            src = link['href']
-            if 'picAll' in src:
-                content_sub = requests.get(src, cookies = cookie).content
-                soup_sub = BeautifulSoup(content_sub, "html.parser")
-                download_one_page(soup_sub)
+        for page in range(1, page_num + 1):
+            url = 'http://weibo.cn/u/%s?filter=1&page=%d' % (user_id, page)
+            content = requests.get(url, cookies = cookie).content
+            soup = BeautifulSoup(content, "html.parser")
+            download_one_page(soup, user_id)
 
-        print('done.')
+            # more pic link, load and download again
+            img_links = soup.find_all('a')
+            for link in img_links:
+                src = link['href']
+                if 'picAll' in src:
+                    content_sub = requests.get(src, cookies = cookie).content
+                    soup_sub = BeautifulSoup(content_sub, "html.parser")
+                    download_one_page(soup_sub, user_id)
+            print('done.')
 
 
-def download_one_page(soup):
+def download_one_page(soup, user_id):
     '''
     download all pictures on the website
     :param soup: web page soup
+    :param user_id: the weibo user id
     :return: None
     '''
     img_urls = soup.find_all('img')
@@ -61,10 +65,11 @@ def download_one_page(soup):
         # use hash value as the image name to avoid duplicate downloading
         name = get_img_name(image_src.content)
         if name not in exists:
-            with open('./downloads/%s.jpg' % name, 'wb') as jpg:
+            with open('./downloads/{}/{}.jpg'.format(user_id, name), 'wb') as jpg:
                 jpg.write(image_src.content)
                 exists.append(name)
                 print('downloading...')
+        print('one page downloaded.')
 
 
 def get_img_name(content):
